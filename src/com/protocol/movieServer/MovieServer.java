b0090f53ec882048a43da93feb229a1a730243e9
wowns1484 <74371211+wowns1484@users.noreply.github.com>
2020-11-16 21:24:35 +0900

    프로토콜 타입 재정의 회원가입 네트워크 코드 추가 회원가입 연결로 인한 해당 부분 서버 코드 수정

--- src/com/protocol/movieServer/MovieServer.java
+++ src/com/protocol/movieServer/MovieServer.java
@@ -19,8 +19,7 @@ public class MovieServer extends Thread {
 	BufferedReader br = null;
 	BufferedWriter bw = null;
 
-	public MovieServer(Socket socket) throws ClassNotFoundException, SQLException 
-	{
+	public MovieServer(Socket socket) throws ClassNotFoundException, SQLException {
 		this.socket = socket;
 		currentID = "NULL";
 		admit = false;
@@ -29,115 +28,103 @@ public class MovieServer extends Thread {
 	}
 
 	@Override
-	public void run() 
-	{
-		try 
-		{
+	public void run() {
+		try {
 			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
 			writePacket(Protocol.SC_REQ_LOGIN);
 			boolean program_stop = false;
 
-			while (true) 
-			{
+			while (true) {
 				String packet = br.readLine();
 				String packetArr[] = packet.split("/");
 				String packetType = packetArr[0];
 
-				switch (packetType) 
+				switch (packetType) {
+				case Protocol.PT_EXIT: // 프로그램 종료 수신
 				{
-					case Protocol.PT_EXIT: // 프로그램 종료 수신
-					{
-						writePacket(Protocol.PT_EXIT);
-						program_stop = true;
-						System.out.println("서버종료");
-						break;
-					}
-					case Protocol.CS_REQ_LOGIN: // 로그인 정보 수신
-					{
-						System.out.println("클라이언트가  로그인 정보를 보냈습니다");
-						String login_id = packetArr[1];
-						String login_passwd = packetArr[2];
-						try 
-						{
-							MemberDAO mDao = new MemberDAO();
-							MemberDTO mDto = mDao.getMember(login_id, login_passwd);
+					writePacket(Protocol.PT_EXIT);
+					program_stop = true;
+					System.out.println("서버종료");
+					break;
+				}
+				case Protocol.CS_REQ_LOGIN: // 로그인 정보 수신
+				{
+					System.out.println("클라이언트가  로그인 정보를 보냈습니다");
+					String login_id = packetArr[1];
+					String login_passwd = packetArr[2];
+					try {
+						MemberDAO mDao = new MemberDAO();
+						MemberDTO mDto = mDao.getMember(login_id, login_passwd);
 
-							if (mDto.getRole().equals("1"))
-								writePacket(Protocol.SC_RES_LOGIN + "/1");
-							else
-								writePacket(Protocol.SC_RES_LOGIN + "/2");
-							System.out.println("로그인 성공"); // 성공시 인터페이스 홈 접속
-							
-						} 
-						catch (Exception e) 
-						{
-							writePacket(Protocol.SC_RES_LOGIN + "/3");
-							System.out.println("로그인 실패"); // 실패시 메시지 창 출력 및 재입력 유도
-						}
-						System.out.println("로그인 처리 결과 전송");
-						break;
+						if (mDto.getRole().equals("1"))
+							writePacket(Protocol.SC_RES_LOGIN + "/1");
+						else
+							writePacket(Protocol.SC_RES_LOGIN + "/2");
+						System.out.println("로그인 성공"); // 성공시 인터페이스 홈 접속
+
+					} catch (Exception e) {
+						writePacket(Protocol.SC_RES_LOGIN + "/3");
+						System.out.println("로그인 실패"); // 실패시 메시지 창 출력 및 재입력 유도
 					}
-					case Protocol.CS_REQ_SIGNUP:
-					{
+					System.out.println("로그인 처리 결과 전송");
+					break;
+				}
+				case Protocol.CS_REQ_SIGNUP: {
+					try {
 						System.out.println("클라이언트가  회원가입 정보를 보냈습니다");
 						String role = packetArr[1]; // 구분(사용자, 관리자)
 						String signUp_id = packetArr[2]; // 아이디
 						String signUp_password = packetArr[3]; // 암호
-						String name = packetArr[4]; // 이름
-						String gender = packetArr[5]; // 성별
+						String account = packetArr[4]; // 계좌 번호
+						String name = packetArr[5]; // 이름
 						String phone_number = packetArr[6]; // 연락처
 						String birth = packetArr[7];
-						String account = packetArr[8]; // 계좌 번호
+						String gender = packetArr[8]; // 성별
+						
 						MemberDAO signUpDAO = new MemberDAO();
 						signUpDAO.addMember(new MemberDTO(signUp_id, role, signUp_password, account, name, phone_number,
 								birth, gender));
-						writePacket(Protocol.SC_RES_SIGNUP);
+						writePacket(Protocol.SC_RES_SIGNUP + "/1");
 						break;
-					}	
+					} catch (Exception e) {
+						e.printStackTrace();
+						writePacket(Protocol.SC_RES_SIGNUP + "/2");
+						break;
+					}
+				}
 				}// end switch
 
 				if (program_stop)
 					break;
 			} // end while
-		} 
-		catch (IOException e) 
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
-		} 
-		catch (Exception e) 
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		try 
-		{
+		try {
 			currUser--;
 			socket.close();
 			System.out.println("[" + socket.getInetAddress() + ":" + socket.getPort() + "]" + "가 접속종료 하였습니다.");
 			System.out.println("현재 사용자 수:" + currUser);
-		} 
-		catch (IOException e) 
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void writePacket(String source) throws Exception {
-		try 
-		{
+		try {
 			bw.write(source);
 			bw.newLine();
 			bw.flush();
-		} 
-		catch (Exception e) 
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public Socket getSocket() 
-	{
+	public Socket getSocket() {
 		return socket;
 	}
 }
--- src/com/view/SignUp.java
+++ src/com/view/SignUp.java
@@ -7,6 +7,8 @@ import java.time.format.*;
 import java.sql.*;
 
 import com.db.model.*;
+import com.main.mainGUI;
+import com.protocol.Protocol;
 
 import javafx.beans.value.*;
 import javafx.collections.*;
@@ -23,103 +25,110 @@ import javafx.scene.input.*;
 
 import com.db.model.*;
 
-public class SignUp 
-{
+public class SignUp {
 
-    @FXML
-    private TextField tf_id;
+	@FXML
+	private TextField tf_id;
 
-    @FXML
-    private PasswordField pf_passwd;
+	@FXML
+	private PasswordField pf_passwd;
 
-    @FXML
-    private TextField tf_name;
+	@FXML
+	private TextField tf_name;
 
-    @FXML
-    private TextField tf_account;
+	@FXML
+	private TextField tf_account;
 
-    @FXML
-    private TextField tf_phone;
+	@FXML
+	private TextField tf_phone;
 
-    @FXML
-    private MenuButton mb_gender;
+	@FXML
+	private MenuButton mb_gender;
 
-    @FXML
-    private MenuItem mi_male;
+	@FXML
+	private MenuItem mi_male;
 
-    @FXML
-    private MenuItem mi_female;
+	@FXML
+	private MenuItem mi_female;
 
-    @FXML
-    private Button btn_sign_up;
+	@FXML
+	private Button btn_sign_up;
 
-    @FXML
-    private DatePicker dp_birth;
-    
-    @FXML
-    private Text t_result;
+	@FXML
+	private DatePicker dp_birth;
 
-    //메뉴 선택시 외부 메뉴 이름 변경
-    @FXML
-    void setFemale(ActionEvent event) 
-    {
-        mb_gender.setText(mi_female.getText());
-    }
+	@FXML
+	private Text t_result;
 
-    //메뉴 선택시 외부 메뉴 이름 변경
-    @FXML
-    void setMale(ActionEvent event) 
-    {
-        mb_gender.setText(mi_male.getText());
-    }
+	// 메뉴 선택시 외부 메뉴 이름 변경
+	@FXML
+	void setFemale(ActionEvent event) {
+		mb_gender.setText(mi_female.getText());
+	}
 
-    //회원가입 시도
-    @FXML
-    void trySignUp(ActionEvent event) 
-    {
-        try
-        {
-            String gender;
-            //성별에 따라 gender 값 세팅
-            if(mb_gender.getText().equals("남"))
-            {
-                gender = "1";
-            }
-            else
-            {
-                gender = "0";
-            }
-            //DatePicker의 값을 원하는 포맷으로 변경
-            DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            MemberDAO mDAO = new MemberDAO();
-            mDAO.addMember(new MemberDTO(tf_id.getText(), "2", pf_passwd.getText(), tf_account.getText(), tf_name.getText(), tf_phone.getText(), dateFormat.format(dp_birth.getValue()), gender));
+	// 메뉴 선택시 외부 메뉴 이름 변경
+	@FXML
+	void setMale(ActionEvent event) {
+		mb_gender.setText(mi_male.getText());
+	}
 
-            t_result.setText("회원가입 성공! 2초 후 로그인 화면으로 돌아갑니다!");
+	// 회원가입 시도
+	@FXML
+	void trySignUp(ActionEvent event) {
+		try {
+			t_result.setText("");
+			String id = tf_id.getText();
+			String passwd = pf_passwd.getText();
+			String account = tf_account.getText();
+			String name = tf_name.getText();
+			String phone_number = tf_phone.getText();
+			String birth = dp_birth.getValue().toString();
+			String gender;
+			// 성별에 따라 gender 값 세팅
+			if (mb_gender.getText().equals("남")) {
+				gender = "1";
+			} else {
+				gender = "0";
+			}
 
-            //스레드사용으로 1.5초 후 로그인 페이지로 전환
-            new Thread(() -> {
-                Platform.runLater(() -> { 
-                    try  
-                    {
-                        Thread.sleep(1500);
-                        Parent root = FXMLLoader.load(SignUp.class.getResource("../view/xml/login.fxml"));
-                        Scene scene = new Scene(root, 600, 400);
-                        Stage primaryStage = (Stage) btn_sign_up.getScene().getWindow();
-                        primaryStage.setTitle("로그인");
-                        primaryStage.setResizable(false);
-                        primaryStage.setScene(scene);
-                        primaryStage.show();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });   
-            }).start();
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-        
-    }
+			mainGUI.writePacket(Protocol.CS_REQ_SIGNUP + "/2/" + id + "/" + passwd + "/" + account + "/" + name + "/"
+					+ phone_number + "/" + birth + "/" + gender);
 
+			String packet = mainGUI.readLine();
+			String packetArr[] = packet.split("/");
+			String packetType = packetArr[0];
+
+			if (packetType.equals(Protocol.SC_RES_SIGNUP)) {
+				String result = packetArr[1];
+				switch (result) {
+				case "1":
+					t_result.setText("회원가입 성공! 2초 후 로그인 화면으로 돌아갑니다!");
+
+					// 스레드사용으로 1.5초 후 로그인 페이지로 전환
+					new Thread(() -> {
+						Platform.runLater(() -> {
+							try {
+								Thread.sleep(1500);
+								Parent root = FXMLLoader.load(SignUp.class.getResource("../view/xml/login.fxml"));
+								Scene scene = new Scene(root, 600, 400);
+								Stage primaryStage = (Stage) btn_sign_up.getScene().getWindow();
+								primaryStage.setTitle("로그인");
+								primaryStage.setResizable(false);
+								primaryStage.setScene(scene);
+								primaryStage.show();
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						});
+					}).start();
+					return;
+				case "2":
+					t_result.setText("회원가입 실패!");
+					return;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
