3cb8a43d5438bd59b77a2e57e9681e6fae6b743a
82103 <82103@DESKTOP-LS94SHP>
2020-11-18 16:32:15 +0900

    에약정보 관리 부분 파일 추가 및 변경

--- src/com/view/ReservationListManage.java
+++ src/com/view/ReservationListManage.java
@@ -7,7 +7,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 
-public class ReservationListRemove {
+public class ReservationListManage {
 
 	 @FXML
 	    private TableView<?> tv_reservation;
