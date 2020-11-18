2b2b95053baa4fe08c301bcd00c469399b2e7a01
82103 <82103@DESKTOP-LS94SHP>
2020-11-18 16:56:12 +0900

    예약정보 수정임당

--- src/com/view/ReservationListChange.java
+++ src/com/view/ReservationListChange.java
@@ -1,5 +1,49 @@
 package com.view;
 
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ReservationListChange {
 
+	 @FXML
+	    private TableView<?> tv_reservation;
+
+	    @FXML
+	    private TableColumn<?, ?> tc_user;
+
+	    @FXML
+	    private TableColumn<?, ?> tc_movie;
+
+	    @FXML
+	    private TableColumn<?, ?> tv_theater;
+
+	    @FXML
+	    private TableColumn<?, ?> tc_screen;
+
+	    @FXML
+	    private TableColumn<?, ?> tc_date;
+
+	    @FXML
+	    private TableColumn<?, ?> tc_start_time;
+
+	    @FXML
+	    private TableColumn<?, ?> tc_end_time;
+
+	    @FXML
+	    private TableColumn<?, ?> tc_seat;
+
+	    @FXML
+	    private TableColumn<?, ?> tc_price;
+
+	    @FXML
+	    private Button btn_change;
+
+	    @FXML
+	    void getChange(ActionEvent event) {
+
+	    }
+	    
 }
